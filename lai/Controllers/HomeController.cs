13a013af679dd24c2a38ba20c99234d562c7a160
thin vuong <vuongvithin@gmail.com>
2023-04-07 15:24:20 +0700

    Add project files.

---
+++ lai/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using lai.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace lai.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(uint soTienGui, double laiSuatGui, uint kyHanGui)
+        {
+            try
+            {
+                uint tien_lai = (uint)(soTienGui * laiSuatGui * kyHanGui / 1200);
+                uint tong_tien = (uint)(soTienGui + tien_lai);
+                ViewData["So_tien_gui"] = soTienGui;
+                ViewData["Lai_suat_gui"] = laiSuatGui;
+                ViewData["Ky_han_gui"] = kyHanGui;
+                ViewData["Tien_lai"] = tien_lai;
+                ViewData["Tong_tien"] = tong_tien;
+            }
+            catch (FormatException)
+            {
+                ViewData["ThongBao"] = "Bạn nhập sai dữ liệu";
+            }
+            catch (Exception ex)
+            {
+                ViewData["ThongBao"] = $"Lỗi: {ex.Message}\tThông tin lỗi: {ex.ToString}";
+            }
+            return View();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ lai/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace lai.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
